2ca381b889d8d5179374b5ba6e4c835ddc5d66d9
Ladivcr <vrosas832@gmail.com>
2021-05-01 23:23:16 -0500

    Creación de app

--- platzigram/platzigram/settings.py
+++ platzigram/platzigram/settings.py
@@ -38,12 +38,15 @@ ALLOWED_HOSTS = []
 # Application definition
 # ! Cuales son las aplicaciones que estan ligadas a nuestro proyecto con django
 INSTALLED_APPS = [
+    # Django apps 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Local apps
+    'posts', 
 ]
 
 # ! Lo mismo que INSTALLED_APPS
--- platzigram/platzigram/urls.py
+++ platzigram/platzigram/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from platzigram import views
-
+from platzigram import views as local_views
+from posts import views as post_views
 #urlpatterns = [
     #path('admin/', admin.site.urls),
 #]
@@ -25,6 +25,9 @@ from platzigram import views
 # * hello-world es la url y hello_world es la respuesta o 
 # * lo que se muestra al ir a esa url
 urlpatterns = [
-    path('hello-world/', views.hello_world),
-    path('hi/', views.hi),
+    path('hello-world/', local_views.hello_world),
+    path('sorted/', local_views.sort_integers),
+    path('hi/<str:name>/<int:age>/', local_views.say_hi), 
+
+    path('posts/', post_views.list_posts)
 ]
